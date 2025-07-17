93f8e65b4f7248650ef26a56fc042dafc29a36a4
piotrtrybus <125976101+piotrtrybus@users.noreply.github.com>
2025-07-17 11:15:48 +0200

    Update config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+OPTIMOVE_API_KEY = os.getenv("OPTIMOVE_API_KEY", "your_optimove_api_key")
+OPTIMOVE_BASE_URL = os.getenv("OPTIMOVE_BASE_URL", "https://api.optimove.net")
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "your_telegram_bot_token")
